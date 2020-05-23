f2ba44b239811c44a954f1cd7450bea7bcf9ffb2
rifayetazamtalukder <rifayetewucse@gmail.com>
2020-05-23 16:39:36 +0600

    firestore configuration settings added in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCVbdv2TbzxGDzOCOFgVWuWmum3ePdfgXc",
+    authDomain: "cgpa-calculator-angular.firebaseapp.com",
+    databaseURL: "https://cgpa-calculator-angular.firebaseio.com",
+    projectId: "cgpa-calculator-angular",
+    storageBucket: "cgpa-calculator-angular.appspot.com",
+    messagingSenderId: "85609185814",
+    appId: "1:85609185814:web:0d350bbd0fce09ecf16b37",
+    measurementId: "G-Y6ER7M1ZRZ"
+  }
 };
